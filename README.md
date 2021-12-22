62086b9b8acab9525dd9d8e926e1bc0313e01c50
Ian <mullane.ian@outlook.com>
2021-12-22 08:46:31 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#nft punk mini web app
+
+This was a test app built to give mysekf a little intro into blockchain.
+
+For some reasons I don't know... I think i didn't code this site well
+
+So, it's not very fast: 
+
+Enjoy my first blockchain thingy: https://chocoscoding-nft-min.netlify.app/
+
+CREDITS: @cleverprogrammer
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import Header from './Components/Header';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import PunkList from './Components/PunkList';
+import Main from './Components/Main';
+
+function App() {
+
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect( ()=>{
+
+    const getMyNfts = async ()=>{
+
+     const openseaData = await axios('https://nft-backend-app.herokuapp.com/',{
+
+     headers: {
+      'Access-Control-Allow-Origin': '*'
+     }
+     })
+     
+     setPunkListData(openseaData.data.assets)
+    }
+    return getMyNfts()
+  }, [])
+
+  return (
+    <div className='app'>
+      <Header />
+      {
+        punkListData.length > 0 && (
+          <>
+      <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
+      <PunkList punkListData={punkListData} setSelectedPunk={setSelectedPunk}/>
+          </>
+        )
+      }
+    </div>
+
+  );
+}
+
+export default App;
